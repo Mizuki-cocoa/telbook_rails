c0ccb5a32c40cf1dae6ace3c4e706f5f7130ffc2
Mizuki <ne191217@senshu-u.jp>
2020-11-04 14:29:58 +0000

    CSS完了,課題達成

--- db/seeds/development/friends.rb
+++ db/seeds/development/friends.rb
@@ -1,7 +1,7 @@
 fnames = ["佐藤", "鈴木", "高橋", "田中"]
 gnames = ["太郎", "次郎", "花子", "夏子"]
-0.upto(3) do |idx|
+0.upto(29) do |idx|
     Friend.create(
-        name: "#{fnames[idx]} #{gnames[idx]}",
+        name: "John#{idx+1}",
     )
-end
+  end
